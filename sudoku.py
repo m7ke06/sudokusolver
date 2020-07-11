bace077482adbdf80090bcd94907a085f8863336
Michal Beneš <20gbenes@dgkralupy.eu>
2020-07-11 19:56:21 +0200

    first

---
+++ sudoku.py
@@ -0,0 +1,79 @@
+digits = [1,2,3,4,5,6,7,8,9]
+digits_inline = [0 for i in range(9)]
+digits_line = 0
+digits_incolumn = [0 for i in range(9)]
+digits_column = 0
+rotated = [[0 for i in range(9)] for j in range(9)]
+same = [[0 for i in range(9)] for j in range(9)]
+
+putin = [ [0,2,0,4,5,6,7,8,9],
+          [4,5,7,0,8,0,2,3,6],
+          [6,8,9,2,3,7,0,4,0],
+          [0,0,5,3,6,2,9,7,4],
+          [2,7,4,0,9,0,6,5,3],
+          [3,9,6,5,7,4,8,0,0],
+          [0,4,0,6,1,8,3,9,7],
+          [7,6,1,0,4,0,5,2,8],
+          [9,3,8,7,2,5,0,6,0],]     # input SUDOKU
+
+# Filling part
+def Fill(putin, rotated):
+    for i in range(9):
+        for j in range(9):
+            rotated[j][i] = putin[i][j]
+
+def Diff(li1, li2): 
+    li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2] 
+    return li_dif 
+
+while True:
+    
+    for i in range(9):
+        digits_line = 0
+        for j in range(9):
+            if putin[i][j] != 0:
+                digits_line = digits_line + 1
+        digits_inline[i] = digits_line
+
+    for i in range(9):
+        digits_column = 0
+        for j in range(9):
+            if putin[j][i] != 0:
+                digits_column = digits_column + 1
+        digits_incolumn[i] = digits_column
+
+
+    for i in range(9):
+        if digits_inline[i] == 8:
+            special = Diff(digits,putin[i])
+            special.remove(0)
+            special_integer = int(special[0])
+            for j in range(9):
+                if putin[i][j] == 0:
+                    putin[i][j] = special_integer
+    Fill(putin,rotated)
+
+    for i in range(9):
+        if digits_incolumn[i] == 8:
+            special = Diff(digits,rotated[i])
+            if 0 in special:
+                special.remove(0)
+                special_integer = int(special[0])
+                for j in range(9):
+                    if putin[j][i] == 0:
+                        putin[j][i] = special_integer
+
+    
+    Fill(putin,rotated)
+    if same == rotated:
+        break
+    
+    same = rotated
+
+for i in range(9):
+    print(putin[i])
+# /Filling part
+
+# Guessing part
+
+
